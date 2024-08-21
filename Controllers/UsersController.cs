255ff618aa83c97a22700ebdb2f78cb554aaa26d
Boray Gulpinar <boray112@gmail.com>
2024-08-21 15:10:37 +0300

    commit userInfo

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,13 +1,15 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using IdentityApp.Models;
+using IdentityApp.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IdentityApp.Controllers
 {
     public class UsersController : Controller
     {
-        private UserManager<IdentityUser> _userManager;
+        private UserManager<AppUser> _userManager;
 
-        public UsersController(UserManager<IdentityUser> userManager)
+        public UsersController(UserManager<AppUser> userManager)
         {
             _userManager = userManager;
         }
@@ -15,6 +17,39 @@ namespace IdentityApp.Controllers
         public IActionResult Index()
         {
             return View(_userManager.Users);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new AppUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    FullName = model.FullName
+                };
+
+                IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                foreach (IdentityError err in result.Errors)
+                {
+                    ModelState.AddModelError("", err.Description);
+                }
+
+            }
+            return View(model);
         }
     }
 }
